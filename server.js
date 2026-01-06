8f10bcd4dfcc4dba2580198d5ced694d33e7fe8f
Utkarsh <utkarshum7@gmail.com>
2026-01-07 00:55:02 +0530

    Adding scalability using kubernetes

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ app.get('/', (req, res) => res.json({ message: 'Hello from Node CI/CD Demo!' }))
 app.get('/health', (req, res) => res.json({ status: 'OK', time: new Date() }));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server on port ${port}`));
-
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 module.exports = app;  // Export for tests
